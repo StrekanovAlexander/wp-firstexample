3dc99acfe332e6ca29647f41f3c1274ab231defd
Alexander Strekanov <strekanov.alexander@gmail.com>
2022-10-21 22:11:47 +0300

    added: register styles and scripts functional

--- functions.php
+++ functions.php
@@ -3,4 +3,13 @@
  * Functions file
  * @package First Example
 */
-?>
+
+add_action('wp_enqueue_scripts', 'theme_register_styles');
+function theme_register_styles() {
+    wp_enqueue_style('style', get_stylesheet_uri());
+}
+
+add_action('wp_footer', 'theme_register_scripts');
+function theme_register_scripts() {
+    wp_enqueue_script('main', get_template_directory_uri() . '/assets/js/main.js');
+}
